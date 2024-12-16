6d561a987a6a5f2f816348b4dd1b3bcc888453de
kovalev Andrey <andremae10@gmail.com>
2024-12-16 18:23:35 +0300

    chore(rust): separate generate pixel matrix into own file

--- src-tauri/src/method.rs
+++ src-tauri/src/method.rs
@@ -114,26 +114,6 @@ fn calculate_required_pixels(size: u64, bits_per_channel: u8, channel_amount: u6
     }
 }
 
-pub fn image_matrix(image: DynamicImage, file_name: &str) {
-    let mut container = ImageBuffer::new(image.width(), image.height());
-
-    for (x, y, pixel) in image.pixels() {
-        container.put_pixel(x, y, map_pixel(pixel));
-    }
-
-    container.save(file_name).unwrap();
-}
-
-fn map_pixel(mut pixel: Rgba<u8>) -> Rgba<u8> {
-    let channels = pixel.channels_mut();
-
-    channels[0] = if (channels[0] & 1) == 1 { 255 } else { 0 };
-    channels[1] = if (channels[1] & 1) == 1 { 255 } else { 0 };
-    channels[2] = if (channels[2] & 1) == 1 { 255 } else { 0 };
-
-    pixel
-}
-
         }
     }
 
